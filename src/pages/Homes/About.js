025bffd09c1e8d15e09bb9a0723e19df7c89bee9
Mheyrie <omota4fidelity@gmail.com>
2024-03-26 20:39:47 +0100

    Mobile view done

--- src/pages/Homes/About.js
+++ src/pages/Homes/About.js
@@ -19,8 +19,8 @@ function About() {
           <p className="about_section_description"> 
           I'm Mary Oladele, a Full Stack Developer with a passion for Excellence. I have a background in Electrical Electronics Engineering and Metallurgy(Gold Mining), I've immersed myself in learning both Frontend and Backend technologies while working on collaborative and personal projects along the way.
           </p>
-          <p className="about_section_description">My mission is to leverage my skills in other create robust, scalable, and user-friendly applications with the use of latest technologies and best practices. I aim to deliver high-quality solutions that meet the needs of clients and users alike. I am committed to continuous improvement and innovation, with a focus on contributing to the success of projects and driving positive outcomes for businesses and end-users.</p>
-          <p className="about_section_description">
+          <p className="about_section_description one">My mission is to leverage my skills in other create robust, scalable, and user-friendly applications with the use of latest technologies and best practices. I aim to deliver high-quality solutions that meet the needs of clients and users alike. I am committed to continuous improvement and innovation, with a focus on contributing to the success of projects and driving positive outcomes for businesses and end-users.</p>
+          <p className="about_section_description one">
           Outside of coding, you'll find me learning Spanish, Reading, or playing Piano. I enjoy spending time at the beach and love traveling. Fun fact: I love coding.
 
           Let's connect! via <a className="about-linkedin" href="https://linkedin.com/in/oladele-mary"> LinkedIn </a>. I look forward to hearing from you! </p>
