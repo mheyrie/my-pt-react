1b1e59c13e42fe558e3ffbfc984fcecdc280e2c4
Mheyrie <omota4fidelity@gmail.com>
2025-03-02 23:37:37 +0100

    updated

--- src/pages/Homes/ContactMe.js
+++ src/pages/Homes/ContactMe.js
@@ -1,80 +1,101 @@
-import React from 'react'
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function ContactMe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_et4xya4";
+    const templateId = "template_g6anhxg";
+    const publicKey = "acoJ6HOdfRiKemWDV";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      message: message,
+      to_name: "Mary Oladele",
+    };
+
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("SUCCESS!", response);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.log("FAILED...", error);
+      });
+  };
   return (
     <section id="contact" className="contact_section">
-        <div>
-            <h2>Contact Me</h2>
-            <p className="contact_text"><a href="mailto:omota4fidelity@gmail.com">Click here to Send me a direct email</a></p>
-            <p className="contact_text one">Or</p>
-            <br />
-            <p className="contact_text one">Fill the form below </p>
-
+      <div>
+        <h2>Contact Me</h2>
+        <p className="contact_text">
+          <a href="mailto:omota4fidelity@gmail.com">
+            Click here 👉 to Send me a direct email
+          </a>
+        </p>
+        <p className="contact_text one">OR</p>
+        <br />
+        <p className="contact_text one">Fill the form below </p>
+      </div>
+      <form className="contact_form_section" onSubmit={handleSubmit}>
+        <div className="">
+          <label htmlFor="name" className="contact-label">
+            <input
+              type="text"
+              className="contact_input text_md"
+              name="name"
+              id="name"
+              placeholder="Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
         </div>
-        <form className="contact_form_section">
-            <div className="container">
-                <label htmlFor="first-name" className="contact-label">
-                    <span className="text_md">First Name</span>
-                    <input type="text"
-                    className="contact_input text_md"
-                    name="first-name"
-                    id="first-name" 
-                    required/>
-                </label>
-                <label htmlFor="last-name" className="contact-label">
-                    <span className="text_md">Last Name</span>
-                    <input type="text"
-                    className="contact_input text_md"
-                    name="last-name"
-                    id="last-name" 
-                    required/>
-                </label>
-                <label htmlFor="email" className="contact-label">
-                    <span className="text_md">Email</span>
-                    <input type="email"
-                    className="contact_input text_md"
-                    name="email"
-                    id="email" 
-                    required/>
-                </label>
-                <label htmlFor="phone-number" className="contact-label">
-                    <span className="text_md">Phone Number</span>
-                    <input type="phone-number"
-                    className="contact_input text_md"
-                    name="phone-number"
-                    id="phone-number" 
-                    required/>
-                </label>
-            </div>
-                <label htmlFor="choose-topic" className="contact-label">
-                    <span className="text_md">Choose a Topic</span>
-                    <select id="choose-topic" className="contact_input text_md">
-                        <option> Select One...</option>
-                        <option>I have a Job for you</option>
-                        <option>I want to make an enquire</option>
-                        <option>I want to collaborate with you</option>
-                    </select>
-                </label>
-                <label htmlFor="message" className="contact-label">
-                    <span className="text_md">Message</span>
-                    <textarea 
-                        className="contact_input text-md" 
-                        id="message" cols="30" rows="10"
-                        placeholder="Type your message here......" />
-                </label>
-                <label htmlFor="checkbox" className="checkbox-label">
-                    <input type="checkbox"
-                    name="checkbox"
-                    id="checkbox" 
-                    required/>
-                    <span className="text_md">I accept the terms</span>
-                </label>
-                <div>
-                    <button className="btn btn_outline_primary">Submit</button>
-                </div>
-        </form>
-    </section>    
-  )
+        <div>
+          <label htmlFor="email" className="contact-label">
+            <input
+              type="email"
+              className="contact_input text_md"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Your Email"
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message" className="contact-label">
+            <textarea
+              className="contact_input text-md"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Type your message here......"
+            />
+          </label>
+        </div>
+
+        <div>
+          <button className="btn btn_outline_primary" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default ContactMe
+export default ContactMe;
